e2a2b83ba99e30dc7ef6eef6824fbc31a70834e0
Andres Manco <andresmanco@gmail.com>
2018-06-19 06:23:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(p)
   r = (1..100).to_a
   r.each do |n|
-  if p<1
-    return false
-  elsif  (p/n)is_a? Integer
-    return true
-  else
-    return false 
-  end
+    if p<1
+      return false
+    elsif  (p/n)is_a? Integer
+      return true
+    else
+      return false 
+    end
 end
 
