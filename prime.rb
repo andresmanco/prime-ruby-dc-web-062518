cf591197427d98c58ec66316bc94ca5e49597487
Andres Manco <andresmanco@gmail.com>
2018-06-19 06:40:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 def prime?(p)
+  b = true
   r = (2..100).to_a
   r.each do |n|
     if p<1
-      return false
+      b=false
     elsif  (p/n).is_a? Integer && p/n != 0
       return true
     else
-      return false 
+      b=false
     end
+    return b
   end
 end
 
